5bb4e9c87281ec24b21b7138648b09d5e44ef752
ajayprabur <ajayprabur@gmail.com>
2021-05-23 22:32:36 +0530

    Implemented solution for Four balls challenge (OOPs)

---
+++ src/main/java/Ball.java
@@ -0,0 +1,31 @@
+import processing.core.PApplet;
+
+public class Ball extends PApplet {
+
+    public static final int BALL_DIAMETER = 10;
+
+    private int xCoordinate;
+    private int yCoordinate;
+    private int speedPerFrame;
+
+    public Ball(int yCoordinate, int speedPerFrame) {
+        this.yCoordinate = yCoordinate;
+        this.speedPerFrame = speedPerFrame;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void moveRight() {
+        this.xCoordinate += this.speedPerFrame;
+    }
+
+    public void drawBall(FourBallChallenge fourBallChallenge) {
+        fourBallChallenge.ellipse(this.getxCoordinate(), this.getyCoordinate(), BALL_DIAMETER, BALL_DIAMETER);
+    }
+}
---
+++ src/main/java/FourBallChallenge.java
@@ -0,0 +1,34 @@
+import processing.core.PApplet;
+
+public class FourBallChallenge extends PApplet {
+
+    public static final int WINDOW_WIDTH = 640;
+    public static final int WINDOW_HEIGHT = 480;
+
+    private Ball[] balls = new Ball[4];
+
+    public FourBallChallenge() {
+        for (int ballIndex = 0; ballIndex < balls.length; ballIndex++) {
+            int ballNumber = ballIndex + 1;
+            balls[ballIndex] = new Ball(WINDOW_HEIGHT * ballNumber / 5, ballNumber);
+        }
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("FourBallChallenge", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+
+    @Override
+    public void draw() {
+        for (Ball ball : balls) {
+            ball.drawBall(this);
+            ball.moveRight();
+        }
+    }
+}
